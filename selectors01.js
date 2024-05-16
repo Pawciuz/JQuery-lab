c36f3e7b679383c29e9c1e799328d89a1ad6fdc8
play202202 <play202202@gmail.com>
2024-05-16 11:13:05 +0000

    task 4.5.2

--- selectors01.js
+++ selectors01.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     $("#button_01").click(function () {
-        $("p").hide();
+        $("p").hide(500)
     })
     $("#button_02").click(function () {
         $("#p_03").hide();
     })
     $("#button_03").click(function () {
         $(".cp_01").hide();
+    })
+    $("#button_04").click(function () {
+        $("p").show();
+    })
+    $("#button_05").click(function () {
+        $(".p3").toggle();
     })
     $("#btn__1").click(function () {
         $("ul li:first").hide();
